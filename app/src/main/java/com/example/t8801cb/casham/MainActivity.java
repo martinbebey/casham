e000fd563d212068e6014a1d567ddde59a9d92e0
Cyrille Martin Bebey-Tongo <Cyrille.BebeyTongo@external.fcagroup.com>
2019-07-04 02:05:28 -0400

    latest tentative layout

--- app/src/main/java/com/example/t8801cb/casham/MainActivity.java
+++ app/src/main/java/com/example/t8801cb/casham/MainActivity.java
@@ -2,10 +2,13 @@ package com.example.t8801cb.casham;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
+import android.content.Intent;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
+    public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
     // Used to load the 'native-lib' library on application startup.
     static {
         System.loadLibrary("native-lib");
@@ -20,10 +23,21 @@ public class MainActivity extends AppCompatActivity {
 //        TextView tv = (TextView) findViewById(R.id.sample_text);
 //        tv.setText(stringFromJNI());
     }
-
     /**
      * A native method that is implemented by the 'native-lib' native library,
      * which is packaged with this application.
      */
     public native String stringFromJNI();
+
+
+    /** Called when the user taps the Send button */
+    public void sendMoney(View view)
+    {
+        // Do something in response to button
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.editText);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
 }
